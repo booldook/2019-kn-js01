0ed348e097b12e38c3d090de1e9dc4e3d153851f
Daeseang <eotmdzla@gmail.com>
2019-02-07 13:45:27 +0900

    if 구문 체크 작업중

--- js/index4.js
+++ js/index4.js
@@ -14,5 +14,48 @@ function sendForm() {
   var gender = f.gender;
   var favorite = f.favorite;
   var mystory = f.mystory;
-  
+  if(uid.value == "") {
+    alert("아이디를 입력하세요.");
+    uid.focus();
+    return;
+  }
+  if(uid.value.length < 8) {
+    alert("아이디는 8글자 이상이어야 합니다.");
+    uid.focus();
+    return;
+  }
+  if(pw1.value == "") {
+    alert("패스워드를 입력하세요.");
+    pw1.focus();
+    return;
+  }
+  if(pw2.value == "") {
+    alert("패스워드를 입력하세요.");
+    pw2.focus();
+    return;
+  }
+  if(pw1.value.length < 8) {
+    alert("패스워드는 8글자 이상입니다.");
+    pw1.focus();
+    return;
+  }
+  if(pw2.value.length < 8) {
+    alert("패스워드는 8글자 이상입니다.");
+    pw2.focus();
+    return;
+  }
+  if(pw1.value != pw2.value) {
+    alert("패스워드가 일치하지 않습니다.");
+    pw1.value = "";
+    pw2.value = "";
+    pw1.focus();
+    return;
+  }
+  if(uname.value == "") {
+    alert("이름을 입력하세요");
+    uname.focus();
+    return;
+  }
+  f.submit();
+  //log(uid.value.length);
 }
