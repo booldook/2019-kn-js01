7cbd3a13e1445bb6c6e9ed6d58b3514e26d10f07
Daeseang <eotmdzla@gmail.com>
2019-02-07 11:50:29 +0900

    icon 추가

---
+++ js/index4.js
@@ -0,0 +1,18 @@
+const log = console.log;
+
+function sendForm() {
+  var f = document.join_form;
+  var uid = f.uid;
+  var pw1 = f.pw1;
+  var pw2 = f.pw2;
+  var uname = f.uname;
+  var email1 = f.email1;
+  var email2 = f.email2;
+  var tel1 = f.tel1;
+  var tel2 = f.tel2;
+  var tel3 = f.tel3;
+  var gender = f.gender;
+  var favorite = f.favorite;
+  var mystory = f.mystory;
+  
+}
